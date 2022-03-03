55066206c9026c9152332cb19d4b6a66eb339637
kv-ooak <>
2022-03-03 12:13:05 +0100

    v0.2.4

--- README.md
+++ README.md
@@ -1,35 +1,5 @@
-# use-sort-by-array
-Sort an array of objects by selected property value, using the values of another array.
+# sort-by-array
+Sort an array of objects or sub-arrays using a reference array.
 
-# Examples
-
-```js
-import useSortByArray from 'use-sort-by-array';
-
-const nameArray = [ 
-  { name: 'Anne', order: 'a' },
-  { name: 'Bob', order: 'b' },
-  { name: 'Henry', order: 'b' },
-  { name: 'Andrew', order: 'd' },
-  { name: 'Jason', order: 'c' },
-  { name: 'Thomas', order: 'b' },
-]
-
-const sortingArray = [ 'b', 'c', 'b', 'b', 'a', 'd' ]
-
-const sortedNames = useSortByArray(nameArray, sortingArray, 'order');
-
-// Output, sorted by the order of sortingArray:
-// sortedNames = [    
-//   { name: 'Bob'    , order: 'b' },
-//   { name: 'Jason'  , order: 'c' },
-//   { name: 'Henry'  , order: 'b' },
-//   { name: 'Thomas' , order: 'b' },
-//   { name: 'Anne'   , order: 'a' },
-//   { name: 'Andrew' , order: 'd' },
-// ]
-
-```
-
+Examples and documentation: https://kvooak.io
 ### Contact me at me@kvooak.io
-### https://kvooak.io
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,117 @@
-import { useState, useEffect, useCallback } from 'react';
 import get from 'lodash.get';
 
-type TargetObjectValue = NonNullable<Record<string, number | string | null>>;
-type TargetEntryValue = [number | string, number | string];
+type ObjectItem = object;
+type ArrayItem = (number | string)[];
+type TargetItem = ObjectItem | ArrayItem;
+type Target = TargetItem[];
 type ReferenceValue = number | string;
-
-type Target = TargetObjectValue[] | TargetEntryValue[];
 type Reference = ReferenceValue[];
+type MapKey = number | string;
+type PropertyKey = number | string;
 
-type TPropertyKey<T> = T extends TargetObjectValue[] ? string : number;
+// sort by order of appearance of values provided in reference array
+// not respecting duplicate values.
+const sortByNotExact =
+  (ref: Reference, propKey: string | number) => (a: any, b: any) => {
+    let compare = 0;
+    let aValue;
+    let bValue;
 
-export default function useSortByArray<
-  TTarget extends Target,
-  U extends Reference,
-  V extends TPropertyKey<TTarget>,
->(target: TTarget, reference: U, propKey: V): Target | undefined {
-  const [sorted, setSorted] = useState<Target | undefined>(undefined);
-  const sortBy = useCallback(
-    (a, b) => {
-      if (typeof propKey === 'string') {
-        return (
-          reference.indexOf(get(a, propKey)) -
-          reference.indexOf(get(b, propKey))
-        );
-      }
+    if (typeof propKey === 'string') {
+      aValue = get(a, propKey);
+      bValue = get(b, propKey);
+    } else if (typeof propKey === 'number') {
+      aValue = a[propKey];
+      bValue = b[propKey];
+    } else {
+      console.error(
+        `Invalid field type was passed to sortByArray() function: ${typeof propKey}, expected String or Number`,
+      );
+      return compare;
+    }
 
-      if (typeof propKey === 'number') {
-        return reference.indexOf(a[propKey]) - reference.indexOf(b[propKey]);
-      }
+    compare = ref.indexOf(aValue) - ref.indexOf(bValue);
 
-      return 0;
-    },
-    [propKey, reference],
+    return compare;
+  };
+
+const isEntryTarget = (target: Target): target is ArrayItem[] =>
+  Array.isArray((target as ArrayItem[])[0]);
+
+const isNumberPropKey = (propKey: PropertyKey): propKey is number => {
+  return typeof propKey === 'number';
+};
+
+const setMapValue = (
+  map: Map<MapKey, Target>,
+  mapKey: MapKey,
+  item: TargetItem,
+) => {
+  if (map.has(mapKey)) {
+    const oldMapValueOfKey = map.get(mapKey);
+    map.set(mapKey, oldMapValueOfKey!.concat(item));
+  } else {
+    map.set(mapKey, [item]);
+  }
+};
+
+export default function sortByArray(
+  target: Target,
+  ref: Reference,
+  propKey: PropertyKey,
+  exact: null | 'exact',
+): Target | undefined {
+  let sorted = [];
+
+  if (!exact) {
+    sorted = target.slice().sort(sortByNotExact(ref, propKey));
+    return sorted;
+  }
+
+  if (exact === 'exact') {
+    if (target.length !== ref.length) {
+      console.error(
+        `Length of target array and reference array do not match.`,
+      );
+      return;
+    }
+
+    const map = new Map<MapKey, Target>();
+    const refCountMap = new Map<MapKey, number>();
+    const uniqueRef = Array.from(new Set(ref));
+    uniqueRef.forEach((value) => refCountMap.set(value, 0));
+
+    const isEntry = isEntryTarget(target);
+    const isNumber = isNumberPropKey(propKey);
+
+    // correct input type with entry array AND number propKey
+    if (isEntry && isNumber) {
+      target.forEach((item: ArrayItem) => {
+        const mapKey = item[propKey];
+        setMapValue(map, mapKey, item);
+      });
+    }
+
+    // correct input type with object array AND string propKey
+    if (!isEntry && !isNumber) {
+      target.forEach((item: ObjectItem) => {
+        const mapKey = get(item, propKey);
+        setMapValue(map, mapKey, item);
+      });
+    }
+
+    sorted = ref.map((value) => {
+      const index = refCountMap.get(value);
+      const currentValue = map.get(value)![index!];
+      refCountMap.set(value, index! + 1);
+      return currentValue;
+    });
+
+    return sorted;
+  }
+
+  console.error(
+    `Invalid sort option passed to sortByArray() function: ${exact}, expected null or 'exact'`,
   );
-
-  useEffect(() => {
-    const _sorted = target.slice().sort(sortBy);
-    setSorted(_sorted);
-    return () => setSorted(undefined);
-  }, [target, reference, sortBy]);
-
-  return sorted;
+  return;
 }
